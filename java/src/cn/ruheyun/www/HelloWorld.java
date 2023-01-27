bf27da1609e258fcbfd6cd0bf550db5cb8f6ab5d
ruheyun <1654033661@qq.com>
2023-01-27 20:17:40 +0800

    update java

--- java/src/cn/ruheyun/www/HelloWorld.java
+++ java/src/cn/ruheyun/www/HelloWorld.java
@@ -2,6 +2,6 @@ package cn.ruheyun.www;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("12345");
+        System.out.println("12345678");
     }
 }
